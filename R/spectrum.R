170ed685a0482cb5d4bbde4f847fc3127f21fbd5
Kristján Jónasson <jonasson@hi.is>
2024-11-28 09:31:05 +0000

    nyjast

--- R/spectrum.R
+++ R/spectrum.R
@@ -5,9 +5,14 @@
 #' @param ...
 #' @export
 spectrum <- function(x, y, config) {
+  nout = 141
+  str(x$time)
+  str(nout)
+  str(config)
   result <- .Fortran("rx_subroutine",
-                     nx = nrow(x),
-                     ny = nrow(y),
+                     nx = as.integer(nrow(x)),
+                     ny = as.integer(nrow(y)),
+                     nout = as.integer(nout),
                      tx = x$time,
                      x = x$series,
                      ty = y$time,
@@ -17,6 +22,23 @@ spectrum <- function(x, y, config) {
                      hifac = config$hifac,
                      n50 = config$n50,
                      alpha = config$alpha,
-                     iwin = config$iwin)
+                     iwin = config$iwin,
+                     rhox = double(1),
+                     rhoy = double(1),
+                     taux = double(1),
+                     tauy = double(1),
+                     dof = double(1),
+                     db6 = double(1),
+                     false_alarm = double(1),
+                     faccritx = double(1),
+                     faccrity = double(1),
+                     alphacritx = double(1),
+                     alphacrity = double(1),
+                     data_x = matrix(0, nout, 12),
+                     data_y = matrix(0, nout, 12),
+                     data_xy = matrix(0, nout, 2),
+                     data_cxy = matrix(0, nout, 7),
+                     data_phxy = matrix(0, nout, 6)
+  )
   str(result)
 }
--- test_spectrum.R
+++ test_spectrum.R
@@ -1,19 +1,26 @@
-
-
 if (Sys.info()['sysname'] == 'Windows') {
   setwd("C:/Users/kbo/Documents/autocross")
-  if (is.loaded('pearsont3sub')) dyn.unload('pearsont3.dll')
-  dyn.load('pearsont3.dll')
+  if (is.loaded('rx_subroutine')) dyn.unload('libredfitx.dll')
+  dyn.load('libredfitx.dll')
 } else {
-  setwd('~/drive/autocross')
-  if (is.loaded('pearsont3sub')) dyn.unload('pearsont3.so')
-  dyn.load('pearsont3.so')
+  setwd('~/autocross')
+  if (is.loaded('rx_subroutine')) dyn.unload('libredfitx.so')
+  dyn.load('libredfitx.so')
 }
 
 x = read.table('x.dat', col.names=c('time', 'series'))
 y = read.table('y.dat', col.names=c('time', 'series'))
+config = list(
+  nsim = 1000,
+  ofac = 4.0,
+  hifac = 1.0,
+  n50 = 7,
+  alpha = 0.05,
+  iwin = 1)
 
-
-source('R/Specrum.R')
-result = Spectrum(x,y,config)
+source('R/spectrum.R')
+str(x)
+str(y)
+str(config)
+result = spectrum(x,y,config)
 str(result)
